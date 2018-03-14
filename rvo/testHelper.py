c1d83195292babc7650557e56d6369c630cd7b3f
redvox27 <v.t.luder@st.hanze.nl>
2018-03-14 22:11:42 +0100

    testhelper classe added

---
+++ rvo/testHelper.py
@@ -0,0 +1,13 @@
+class TestHelper:
+
+    def __init__(self):
+        self.sector_dict = {}
+        self.fill_sector_dict()
+
+    def get_dictionary(self):
+        return self.sector_dict
+
+    def fill_sector_dict(self):
+        self.sector_dict["SDE+"] = "3A27819"
+
+
